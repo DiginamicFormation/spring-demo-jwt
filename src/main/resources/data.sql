7f87d53b0177b58594a80a42ac8909cc4a659219
rbonnamy <rbonnamy@diginamic.fr>
2024-03-21 14:27:19 +0100

    correction pour fonctionner avec h2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,1 @@
-CREATE TABLE utilisateur (ID int NOT NULL PRIMARY KEY, NOM varchar(50) NOT NULL, PRENOM varchar(50) NOT NULL, EMAIL varchar(50) NOT NULL, PASSWORD varchar(200) NOT NULL, ROLE varchar(10) NOT NULL);
--- INSERTIONS
 Insert into utilisateur (ID, NOM, PRENOM, EMAIL, PASSWORD, ROLE) values (1, 'BONNAMY', 'Richard', 'rbonnamy@diginamic.fr','toto','ADMIN') ;
